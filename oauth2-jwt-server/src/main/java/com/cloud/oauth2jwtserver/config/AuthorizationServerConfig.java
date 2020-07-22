2e7329d66c72e73ab66324e00cb6eef775bca0d3
huxingran1993 <57091978+huxingran1993@users.noreply.github.com>
2020-07-22 22:36:07 +0200

    oauth2-jwt-server & redis

---
+++ oauth2-jwt-server/src/main/java/com/cloud/oauth2jwtserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,20 @@
+package com.cloud.oauth2jwtserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+}
